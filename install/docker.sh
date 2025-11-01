5a4835e20b0c08cf05dac87dd8396a6af90f036d
maxenceng <maxence.grosjean@cpe.fr>
2025-11-01 13:28:09 +0100

    Preparation for Dockarr project

---
+++ install/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+apt upgrade
+apt install curl apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update
+apt install docker-ce
+systemctl status docker
